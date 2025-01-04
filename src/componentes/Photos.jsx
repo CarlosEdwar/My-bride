2f4b172916ce6f906c8158770206204ab4b24212
Carlos Barros <rkadu072@gmail.com>
2025-01-04 15:11:37 -0300

    correção fotos

--- src/componentes/Photos.jsx
+++ src/componentes/Photos.jsx
@@ -2,8 +2,8 @@ function Photos() {
     return(
         <div 
             className="max-w-80 min-h-40 w-auto h-80 justify-center flex p-15 gap-4">
-             <img src="src/imgs/Imagem do WhatsApp de 2024-12-25 à(s) 16.36.51_2ff15654.jpg" alt="" />
-             <img src="src\imgs\Imagem do WhatsApp de 2024-12-25 à(s) 16.36.52_a6e62780.jpg" alt="" />
+             <img src="src\imgs\imagem01.png" alt="" />
+             <img src="src\imgs\Imagem02.png" alt="" />
         </div>
 
     )
