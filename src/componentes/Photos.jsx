b8b882aaaa51e70726228c8dd7abcca120678ede
Carlos Barros <rkadu072@gmail.com>
2025-01-11 18:31:45 -0300

    fix: enderecos fotos

--- src/componentes/Photos.jsx
+++ src/componentes/Photos.jsx
@@ -2,8 +2,8 @@ function Photos() {
     return(
         <div 
             className="max-w-80 min-h-40 w-auto h-80 justify-center flex p-15 gap-4">
-             <img src="src\imgs\imagem01.png" alt="" />
-             <img src="src\imgs\Imagem02.png" alt="" />
+             <img src="/imgs/imagem01.png" alt="" />
+             <img src="/imgs/Imagem02.png" alt="" />
         </div>
 
     )
